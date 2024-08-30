c92c61ffa5ed9609dbb263fa87d0043250746811
rrohit1319 <rohit.programmer.tecg@gmail.com>
2024-08-30 20:56:14 +0530

    modified: home_page_completed/index.html modified: home_page_completed/script.js modified: home_page_completed/styles.css

---
+++ script.js
@@ -0,0 +1,18 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+// const searchBar = document.querySelector('.search-bar');
+// const searchInput = document.querySelector('#search-input');
+// const searchButton = document.querySelector('#search-btn');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
